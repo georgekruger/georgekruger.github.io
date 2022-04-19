ee50abac66c076515ba5e6e902e6f217e0ade1c4
George Kruger <georgekruger@hotmail.co.uk>
2022-04-19 03:08:19 +0100

    up2

--- JavaScript/Common.js
+++ JavaScript/Common.js
@@ -1,16 +1,26 @@
+
+var Version = 2;
 /*This is horrific and needs a rework*/
 
 function dropdownmenu(ContentElem) {
     var ClassActive = "dropdown-content-show";
-    if (ContentElem.classList.contains(ClassActive)) {
+    if (ContentElem.classList.contains(ClassActive) && !ContentElem.classList.contains("disable")) {
         ContentElem.classList.remove(ClassActive);
         ContentElem.nextElementSibling.style.height = '0px';
         egegeg(ContentElem, false);
-    } else {
+        ContentElem.classList.add("disable");
+        setTimeout(function () {
+            ContentElem.classList.remove("disable")
+        }, 600);
+    } else if (!ContentElem.classList.contains("disable")) {
         var ContentHeight = ContentElem.nextElementSibling.scrollHeight;
         ContentElem.nextElementSibling.style.height = ContentHeight + 'px';
         ContentElem.classList.add(ClassActive);
         egegeg(ContentElem, true);
+        ContentElem.classList.add("disable");
+        setTimeout(function () {
+            ContentElem.classList.remove("disable")
+        }, 600);
     }
 
     function egegeg(ContentElem, open) {
